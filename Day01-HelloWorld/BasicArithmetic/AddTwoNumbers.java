75427faf84328e8e2142f8e749d62c6f2a88536a
kesha <kesharshaw195@gmail.com>
2026-07-05 20:31:37 +0530

    Day1-Addition Program

---
+++ Day01-HelloWorld/BasicArithmetic/AddTwoNumbers.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args) {
+
+    System.out.println("the sum of no is:");
+
+    int num1=6;
+    int num2=5;
+    int num3=7;
+    int sum = num1 + num2 + num3;
+    System.out.println(sum);
+}
+}
