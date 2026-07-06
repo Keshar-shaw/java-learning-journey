e429195143859c32301f41bd00f966e31ce85df8
kesha <kesharshaw195@gmail.com>
2026-07-06 20:46:17 +0530

    Day 2 - String Input

---
+++ Day02-Userinput/Stringinput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+
+        System.out.println("Enter your name:");
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        System.out.println(str);
+}
+}
