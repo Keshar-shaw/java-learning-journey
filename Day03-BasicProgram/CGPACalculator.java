c2d13807818e7f6b4de1781391792806b8bf5952
kesha <kesharshaw195@gmail.com>
2026-07-10 22:21:54 +0530

    Day 3- CGPA Calculator

---
+++ Day03-BasicProgram/CGPACalculator.java
@@ -0,0 +1,9 @@
+	public class Main{
+        public static void main(String[] args){
+                float subject1 =45;
+                float subject2=75;
+                float subject3 =88;
+               float cgpa=(subject1+subject2+subject3)/30;
+                System.out.println(cgpa);
+        }
+}
