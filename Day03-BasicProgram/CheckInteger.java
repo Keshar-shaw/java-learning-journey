028e0fa4c6a460194e183658506a2d0e126eee71
kesha <kesharshaw195@gmail.com>
2026-07-10 22:27:02 +0530

    Day 3- Check Integer

---
+++ Day03-BasicProgram/CheckInteger.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+        public static void main(String[] args) {
+                System.out.println("Enter the number:");
+                Scanner sc = new Scanner(System.in);
+
+                System.out.println(sc.hasNextInt());
+        }
+}
