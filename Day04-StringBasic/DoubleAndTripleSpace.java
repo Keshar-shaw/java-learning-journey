837cd7c42e536ff9db92095479542ab183515393
kesha <kesharshaw195@gmail.com>
2026-07-14 23:36:14 +0530

    Day 4-Detect Double and Triple Space

---
+++ Day04-StringBasic/DoubleAndTripleSpace.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+
+        String myString = "This string contains double  and triple   index";
+        System.out.println(myString.indexOf("  "));
+        System.out.println(myString.indexOf("   "));
+    }
+}
