8cc7084b8adf2785fb88674b936e4ec369549e6f
kesha <kesharshaw195@gmail.com>
2026-07-14 23:34:43 +0530

    Day 4- Replace space with Underscore

---
+++ Day04-StringBasic/ReplaceString.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args) {
+        String text ="Hi This is Keshar";
+        text = text.replace(" ","_");
+        System.out.println(text);
+    }
+}
